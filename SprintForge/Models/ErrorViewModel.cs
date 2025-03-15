39d8906f7bfeb7075029486aa72d988c30558de3
Patryk Wójtowicz <ultiprogames@gmail.com>
2025-03-15 15:17:40 +0100

    Init

---
+++ SprintForge/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SprintForge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
